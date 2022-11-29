252132b03939e997a83134336380b16906d73e78
Fernando  Lopez Cardenas <fernandolopezcardenas@Fernandos-MacBook-Pro.local>
2022-11-29 00:54:38 -0600

    setting up github actions to deploy into aws

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,12 @@ import { createBot, Bot } from 'whatsapp-cloud-api';
     const webhookVerifyToken : string = process.env.WEBHOOK_VERIFICATION_TOKEN || '';
     const from : string = process.env.PHONE_ID || '';
     const token : string = process.env.TOKEN || '';
-    const to : string = '526141388579';
-
+    
     // Create a bot that can send messages
     const bot : Bot = createBot(from, token);
 
-    // Send text message
-    // const response = await bot.sendText(to, 'Hello world');
-
     // Start express server to listen for incoming messages
-    // NOTE: See below under `Documentation/Tutorial` to learn how
-    // you can verify the webhook URL and make the server publicly available
-    
+    // you can verify the webhook URL and make the server publicly available    
     await bot.startExpressServer({
       webhookVerifyToken,
     });
@@ -29,11 +23,7 @@ import { createBot, Bot } from 'whatsapp-cloud-api';
     bot.on('message', async (msg) => {
       console.log(msg);
 
-      if (msg.type === 'text') {
-        await bot.sendText(msg.from, 'Received your text message!');
-      } else if (msg.type === 'image') {
-        await bot.sendText(msg.from, 'Received your image!');
-      }
+      await bot.sendText(msg.from, 'Received your text message!');
     });
   } catch (err) {
     console.log(err);
